780e43a0aa2077023d0148dd5f1908f143b89f2f
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 01:48:06 +0200

    Render published Home data

--- src/app/[country]/[locale]/(storefront)/page.tsx
+++ src/app/[country]/[locale]/(storefront)/page.tsx
@@ -4,6 +4,7 @@ import { HomePuckRenderer } from "@/components/puck/HomePuckRenderer";
 import { PRODUCT_CARD_FIELDS } from "@/lib/data/cached";
 import { cachedListProducts } from "@/lib/data/products";
 import { generateHomeMetadata } from "@/lib/metadata/home";
+import { getHomePageData } from "@/lib/puck/get-home-data";
 import { getAccessToken } from "@/lib/spree";
 
 interface HomePageProps {
@@ -13,20 +14,14 @@ interface HomePageProps {
   }>;
 }
 
-export async function generateMetadata({
-  params,
-}: HomePageProps): Promise<Metadata> {
+export async function generateMetadata({ params }: HomePageProps): Promise<Metadata> {
   const { country, locale } = await params;
 
-  return generateHomeMetadata({
-    country,
-    locale,
-  });
+  return generateHomeMetadata({ country, locale });
 }
 
 export default async function HomePage({ params }: HomePageProps) {
   const { country, locale } = await params;
-
   const basePath = `/${country}/${locale}`;
   const userToken = await getAccessToken();
 
@@ -34,22 +29,17 @@ export default async function HomePage({ params }: HomePageProps) {
 
   try {
     const response = await cachedListProducts(
-      {
-        limit: 8,
-        fields: PRODUCT_CARD_FIELDS,
-      },
-      {
-        locale,
-        country,
-      },
+      { limit: 8, fields: PRODUCT_CARD_FIELDS },
+      { locale, country },
       "dtc",
       userToken,
     );
-
     products = response.data ?? [];
   } catch (error) {
     console.error("HomePuckRenderer: failed to load products", error);
   }
 
-  return <HomePuckRenderer products={products} basePath={basePath} />;
+  const data = await getHomePageData();
+
+  return <HomePuckRenderer products={products} basePath={basePath} data={data} />;
 }
