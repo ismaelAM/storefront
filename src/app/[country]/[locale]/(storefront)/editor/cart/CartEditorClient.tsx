6ccb8362e19cd2f25d8c1657cfc072e3179c8779
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 02:56:46 +0200

    Add editor navigation to cart editor

--- src/app/[country]/[locale]/(storefront)/editor/cart/CartEditorClient.tsx
+++ src/app/[country]/[locale]/(storefront)/editor/cart/CartEditorClient.tsx
@@ -2,15 +2,27 @@
 
 import { Puck } from "@puckeditor/core";
 import type { Data } from "@puckeditor/core";
+import { EditorSectionNav } from "@/components/puck/EditorSectionNav";
 import { saveSitePageData } from "@/lib/puck/save-site-page-data";
 import { siteConfig } from "@/puck/site-config";
 
-export function CartEditorClient({ initialData }: { initialData: Data }) {
+export function CartEditorClient({
+  initialData,
+  basePath,
+}: {
+  initialData: Data;
+  basePath: string;
+}) {
   return (
-    <Puck
-      config={siteConfig}
-      data={initialData}
-      onPublish={async (data) => saveSitePageData("cart", data)}
-    />
+    <div className="flex h-screen flex-col overflow-hidden">
+      <EditorSectionNav basePath={basePath} />
+      <div className="min-h-0 flex-1">
+        <Puck
+          config={siteConfig}
+          data={initialData}
+          onPublish={async (data) => saveSitePageData("cart", data)}
+        />
+      </div>
+    </div>
   );
 }
