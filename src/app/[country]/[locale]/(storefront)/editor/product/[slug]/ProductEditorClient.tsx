2d74df31518b2fa6b010d498fcb1091bd970b991
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 03:07:26 +0200

    Add shared editor navigation to product editor

--- src/app/[country]/[locale]/(storefront)/editor/product/[slug]/ProductEditorClient.tsx
+++ src/app/[country]/[locale]/(storefront)/editor/product/[slug]/ProductEditorClient.tsx
@@ -2,23 +2,23 @@
 
 import { Puck } from "@puckeditor/core";
 import type { Data } from "@puckeditor/core";
+import { EditorSectionNav } from "@/components/puck/EditorSectionNav";
 import { productConfig } from "@/puck/product-config";
 import { saveProductPageData } from "@/lib/puck/save-product-data";
 
-export function ProductEditorClient({
-  slug,
-  initialData,
-}: {
-  slug: string;
-  initialData: Data;
-}) {
+export function ProductEditorClient({ slug, initialData, basePath }: { slug: string; initialData: Data; basePath: string }) {
   return (
-    <Puck
-      config={productConfig}
-      data={initialData}
-      onPublish={async (data) => {
-        await saveProductPageData(slug, data);
-      }}
-    />
+    <div className="flex h-screen flex-col overflow-hidden">
+      <EditorSectionNav basePath={basePath} />
+      <div className="min-h-0 flex-1">
+        <Puck
+          config={productConfig}
+          data={initialData}
+          onPublish={async (data) => {
+            await saveProductPageData(slug, data);
+          }}
+        />
+      </div>
+    </div>
   );
 }
