6abe2946758033f64225e496d3141822e0e0cfee
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 02:30:42 +0200

    Make Home Puck editor dynamic

--- src/app/[country]/[locale]/(storefront)/editor/page.tsx
+++ src/app/[country]/[locale]/(storefront)/editor/page.tsx
@@ -1,4 +1,5 @@
 import type { Product } from "@spree/sdk";
+import { connection } from "next/server";
 import { PRODUCT_CARD_FIELDS } from "@/lib/data/cached";
 import { getProducts } from "@/lib/data/products";
 import { getHomePageData } from "@/lib/puck/get-home-data";
@@ -12,6 +13,7 @@ interface EditorPageProps {
 }
 
 export default async function EditorPage({ params }: EditorPageProps) {
+  await connection();
   const { country, locale } = await params;
 
   let products: Product[] = [];
