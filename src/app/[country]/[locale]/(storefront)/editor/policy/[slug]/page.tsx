f95555e9439c76d832c5d9915c26e04740cb0939
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 02:30:59 +0200

    Make policy Puck editor dynamic

--- src/app/[country]/[locale]/(storefront)/editor/policy/[slug]/page.tsx
+++ src/app/[country]/[locale]/(storefront)/editor/policy/[slug]/page.tsx
@@ -1,12 +1,16 @@
 import type { Data } from "@puckeditor/core";
+import { connection } from "next/server";
 import { notFound } from "next/navigation";
 import { getPolicy } from "@/lib/data/policies";
 import { getSitePageData } from "@/lib/puck/get-site-page-data";
 import { PolicyEditorClient } from "./PolicyEditorClient";
 
-interface Props { params: Promise<{ country: string; locale: string; slug: string }> }
+interface Props {
+  params: Promise<{ country: string; locale: string; slug: string }>;
+}
 
 export default async function PolicyEditorPage({ params }: Props) {
+  await connection();
   const { country, locale, slug } = await params;
   const policy = await getPolicy(slug, { country, locale });
   if (!policy) notFound();
