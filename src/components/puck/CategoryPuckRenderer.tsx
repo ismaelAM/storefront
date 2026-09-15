6ece30244e7e752775e26a712b7fbfa1cfac26d6
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 02:16:07 +0200

    Provide real products to category Puck

--- src/components/puck/CategoryPuckRenderer.tsx
+++ src/components/puck/CategoryPuckRenderer.tsx
@@ -2,8 +2,22 @@
 
 import { Render } from "@puckeditor/core";
 import type { Data } from "@puckeditor/core";
+import type { Product } from "@spree/sdk";
+import { PuckProductsProvider } from "@/components/puck/PuckProductsContext";
 import { categoryConfig } from "@/puck/category-config";
 
-export function CategoryPuckRenderer({ data }: { data: Data }) {
-  return <Render config={categoryConfig} data={data} />;
+export function CategoryPuckRenderer({
+  data,
+  products,
+  basePath,
+}: {
+  data: Data;
+  products: Product[];
+  basePath: string;
+}) {
+  return (
+    <PuckProductsProvider products={products} basePath={basePath}>
+      <Render config={categoryConfig} data={data} />
+    </PuckProductsProvider>
+  );
 }
