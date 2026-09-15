305a9da59b2222c911d260a1868a7de66ecee76f
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 02:20:56 +0200

    Add visual policy Puck editor

---
+++ src/app/[country]/[locale]/(storefront)/editor/policy/[slug]/PolicyEditorClient.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Puck } from "@puckeditor/core";
+import type { Data } from "@puckeditor/core";
+import { saveSitePageData } from "@/lib/puck/save-site-page-data";
+import { siteConfig } from "@/puck/site-config";
+
+export function PolicyEditorClient({ slug, initialData }: { slug: string; initialData: Data }) {
+  return (
+    <Puck
+      config={siteConfig}
+      data={initialData}
+      onPublish={async (data) => saveSitePageData(`policy:${slug}`, data)}
+    />
+  );
+}
