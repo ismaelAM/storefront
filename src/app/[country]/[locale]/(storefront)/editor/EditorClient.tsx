44f164953626e9a714173ddf2be3ce781969aa83
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 02:56:41 +0200

    Add editor navigation to home editor

--- src/app/[country]/[locale]/(storefront)/editor/EditorClient.tsx
+++ src/app/[country]/[locale]/(storefront)/editor/EditorClient.tsx
@@ -4,6 +4,7 @@ import { Puck } from "@puckeditor/core";
 import type { Data } from "@puckeditor/core";
 import type { Product } from "@spree/sdk";
 import { PuckProductsProvider } from "@/components/puck/PuckProductsContext";
+import { EditorSectionNav } from "@/components/puck/EditorSectionNav";
 import { enhancedConfig } from "@/puck/enhanced-config";
 import { saveHomePageData } from "@/lib/puck/save-home-data";
 
@@ -20,18 +21,22 @@ export function EditorClient({
   locale,
   initialData,
 }: EditorClientProps) {
+  const basePath = `/${country}/${locale}`;
+
   return (
-    <PuckProductsProvider
-      products={products}
-      basePath={`/${country}/${locale}`}
-    >
-      <Puck
-        config={enhancedConfig}
-        data={initialData}
-        onPublish={async (data) => {
-          await saveHomePageData(data);
-        }}
-      />
-    </PuckProductsProvider>
+    <div className="flex h-screen flex-col overflow-hidden">
+      <EditorSectionNav basePath={basePath} />
+      <div className="min-h-0 flex-1">
+        <PuckProductsProvider products={products} basePath={basePath}>
+          <Puck
+            config={enhancedConfig}
+            data={initialData}
+            onPublish={async (data) => {
+              await saveHomePageData(data);
+            }}
+          />
+        </PuckProductsProvider>
+      </div>
+    </div>
   );
 }
