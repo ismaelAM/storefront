b78793d36b3b126fe5c3e8257c4e67f83dc681d3
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 03:07:21 +0200

    Add shared editor navigation to category editor

--- src/app/[country]/[locale]/(storefront)/editor/category/[...permalink]/CategoryEditorClient.tsx
+++ src/app/[country]/[locale]/(storefront)/editor/category/[...permalink]/CategoryEditorClient.tsx
@@ -4,29 +4,25 @@ import { Puck } from "@puckeditor/core";
 import type { Data } from "@puckeditor/core";
 import type { Product } from "@spree/sdk";
 import { PuckProductsProvider } from "@/components/puck/PuckProductsContext";
+import { EditorSectionNav } from "@/components/puck/EditorSectionNav";
 import { categoryConfig } from "@/puck/category-config";
 import { saveCategoryPageData } from "@/lib/puck/save-category-data";
 
-export function CategoryEditorClient({
-  permalink,
-  initialData,
-  products,
-  basePath,
-}: {
-  permalink: string;
-  initialData: Data;
-  products: Product[];
-  basePath: string;
-}) {
+export function CategoryEditorClient({ permalink, initialData, products, basePath }: { permalink: string; initialData: Data; products: Product[]; basePath: string }) {
   return (
-    <PuckProductsProvider products={products} basePath={basePath}>
-      <Puck
-        config={categoryConfig}
-        data={initialData}
-        onPublish={async (data) => {
-          await saveCategoryPageData(permalink, data);
-        }}
-      />
-    </PuckProductsProvider>
+    <div className="flex h-screen flex-col overflow-hidden">
+      <EditorSectionNav basePath={basePath} />
+      <div className="min-h-0 flex-1">
+        <PuckProductsProvider products={products} basePath={basePath}>
+          <Puck
+            config={categoryConfig}
+            data={initialData}
+            onPublish={async (data) => {
+              await saveCategoryPageData(permalink, data);
+            }}
+          />
+        </PuckProductsProvider>
+      </div>
+    </div>
   );
 }
