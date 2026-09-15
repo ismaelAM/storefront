753485e75e1e81626d968848d5e05d3749bfd35b
ismaelAM <63877386+ismaelAM@users.noreply.github.com>
2026-09-15 02:16:20 +0200

    Preview real category products in editor

--- src/app/[country]/[locale]/(storefront)/editor/category/[...permalink]/CategoryEditorClient.tsx
+++ src/app/[country]/[locale]/(storefront)/editor/category/[...permalink]/CategoryEditorClient.tsx
@@ -2,23 +2,31 @@
 
 import { Puck } from "@puckeditor/core";
 import type { Data } from "@puckeditor/core";
+import type { Product } from "@spree/sdk";
+import { PuckProductsProvider } from "@/components/puck/PuckProductsContext";
 import { categoryConfig } from "@/puck/category-config";
 import { saveCategoryPageData } from "@/lib/puck/save-category-data";
 
 export function CategoryEditorClient({
   permalink,
   initialData,
+  products,
+  basePath,
 }: {
   permalink: string;
   initialData: Data;
+  products: Product[];
+  basePath: string;
 }) {
   return (
-    <Puck
-      config={categoryConfig}
-      data={initialData}
-      onPublish={async (data) => {
-        await saveCategoryPageData(permalink, data);
-      }}
-    />
+    <PuckProductsProvider products={products} basePath={basePath}>
+      <Puck
+        config={categoryConfig}
+        data={initialData}
+        onPublish={async (data) => {
+          await saveCategoryPageData(permalink, data);
+        }}
+      />
+    </PuckProductsProvider>
   );
 }
